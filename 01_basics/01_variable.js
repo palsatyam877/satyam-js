ddf654a91652cba508586aeee51dd60b4d2993e1
palsatyam877 <123195762+palsatyam877@users.noreply.github.com>
2024-01-15 05:24:44 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 12234;
+let accountEmail = "sp@google.com";
+var accountpassword = "1234567";
+
+console.table([accountId , accountEmail , accountpassword]);
+
+let Name = "Satyam"; // string
+let age = 23; // number // bigint
+let turn; // undefined
+let _turn = null; // null
+// symbol
+let isPossible = true; // boolean
+// object
+console.table([typeof Name , typeof age , typeof turn , typeof _turn , typeof isPossible]);
+
+let someNumber = "6969";
+
+someNumber = Number(someNumber);
